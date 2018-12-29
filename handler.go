cd4c4a2049411a4fb42bb2e08a926e9a6b867162
KennyTheBard <root@DESKTOP-OIMN845.localdomain>
2018-12-29 23:13:29 +0200

    Rescaling with bilinear interpolation is working ona single gourutine

--- handler.go
+++ handler.go
@@ -54,62 +54,58 @@ func min(a, b uint32) uint32 {
     }
 }
 
+/**
+    Return the interpoled value in a point between the 2
+    v1 - value in first point
+    v2 - value in second point
+    proc - distance between the first point and
+    the interest point raported to the whole distance
+*/
+func linear_interpolation(v1 uint32, v2 uint32, proc float64) uint32 {
+    return uint32(math.Floor((float64(v1) * (1 - proc)) + (float64(v2) * proc)))
+}
+
+func scale_index(i int, r float64) int {
+    return int(math.Floor(float64(i) * r))
+}
+
 func resize(orig image.Image, ret image.Image) {
     orig_bounds := orig.Bounds()
     ret_bounds := ret.Bounds()
 
     // ratio return to original
-    height_ratio := float64(ret_bounds.Max.Y - ret_bounds.Min.Y) / float64(orig_bounds.Max.Y - orig_bounds.Min.Y)
-    width_ratio := float64(ret_bounds.Max.X - ret_bounds.Min.X) / float64(orig_bounds.Max.X - orig_bounds.Min.X)
+    height_ratio := float64(orig_bounds.Max.Y - orig_bounds.Min.Y) / float64(ret_bounds.Max.Y - ret_bounds.Min.Y)
+    width_ratio := float64(orig_bounds.Max.X - orig_bounds.Min.X) / float64(ret_bounds.Max.X - ret_bounds.Min.X)
 
     // n := 10
     // for p := 0; p < n; p ++ {
     //     go func() {
     //         rank := p
 
-            for y := orig_bounds.Min.Y /*+ rank*/; y <= orig_bounds.Max.Y; y ++/*+= n*/ {
-                for x := orig_bounds.Min.X; x <= orig_bounds.Max.X; x++ {
-                    r, g, b, a := orig.At(y, x).RGBA()
+            for y := ret_bounds.Min.Y /*+ rank*/; y <= ret_bounds.Max.Y; y ++/*+= n*/ {
+                for x := ret_bounds.Min.X; x <= ret_bounds.Max.X; x++ {
 
-                    // fmt.Print("[",y,"][",x,"] enters in riles ")
-                    // fmt.Print("from ",uint64(math.Floor(float64(y) * height_ratio))," to ",uint64(math.Ceil(float64(y + 1) * height_ratio)) - 1, " ")
-                    // fmt.Println("and from",uint64(math.Floor(float64(x) * width_ratio)),"to",uint64(math.Ceil(float64(x + 1) * width_ratio)) - 1)
+                     r11, g11, b11, a11 := orig.At(scale_index(x, width_ratio), scale_index(y, height_ratio)).RGBA()
+                     r12, g12, b12, a12 := orig.At(scale_index(x + 1, width_ratio), scale_index(y, height_ratio)).RGBA()
+                     r21, g21, b21, a21 := orig.At(scale_index(x, width_ratio), scale_index(y + 1, height_ratio)).RGBA()
+                     r22, g22, b22, a22 := orig.At(scale_index(x + 1, width_ratio), scale_index(y + 1, height_ratio)).RGBA()
 
-                    for i := uint64(math.Floor(float64(y) * height_ratio)); i < uint64(math.Ceil(float64(y + 1) * height_ratio)); i++ {
-                        for j := uint64(math.Floor(float64(x) * width_ratio)); j < uint64(math.Ceil(float64(x + 1) * width_ratio)); j++ {
-                            nr, ng, nb, na := ret.At(int(i), int(b)).RGBA()
+                     r_aux1 := linear_interpolation(r11, r12, 0.5)
+                     g_aux1 := linear_interpolation(g11, g12, 0.5)
+                     b_aux1 := linear_interpolation(b11, b12, 0.5)
+                     a_aux1 := linear_interpolation(a11, a12, 0.5)
 
-                            proc_h := (math.Min(float64(y + 1) * height_ratio, float64(i + 1)) - math.Max(float64(y) * height_ratio, float64(i))) / height_ratio
-                            proc_w := (math.Min(float64(x + 1) * width_ratio, float64(j + 1)) - math.Max(float64(x) * width_ratio, float64(j))) / width_ratio
+                     r_aux2 := linear_interpolation(r21, r22, 0.5)
+                     g_aux2 := linear_interpolation(g21, g22, 0.5)
+                     b_aux2 := linear_interpolation(b21, b22, 0.5)
+                     a_aux2 := linear_interpolation(a21, a22, 0.5)
 
-                            // if x == 5 && y == 5 {
-                            //     fmt.Println("i =", i)
-                            //     fmt.Println("j =", j)
-                                // fmt.Println("height_ratio =", height_ratio)
-                                // fmt.Println("width_ratio =", width_ratio)
-                                // fmt.Println("proc_h =", proc_h)
-                                // fmt.Println("proc_w =", proc_w)
-                                // fmt.Println("math.Min(float64(y + 1) * height_ratio, float64(i + 1)) =", math.Min(float64(y + 1) * height_ratio, float64(i + 1)))
-                                // fmt.Println("math.Max(float64(y) * height_ratio, float64(i) =", math.Max(float64(y) * height_ratio, float64(i)))
-                                // fmt.Println("math.Min(float64(x + 1) * width_ratio, float64(j + 1)) =", math.Min(float64(x + 1) * width_ratio, float64(j + 1)))
-                                // fmt.Println("math.Max(float64(x) * width_ratio, float64(j)) =", math.Max(float64(x) * width_ratio, float64(j)))
-                                // fmt.Println()
-                            // }
-                            //fmt.Println(nr, "+", uint32(proc_h * proc_w * float64(r)))
+                     r_fin := linear_interpolation(r_aux1, r_aux2, 0.5)
+                     g_fin := linear_interpolation(g_aux1, g_aux2, 0.5)
+                     b_fin := linear_interpolation(b_aux1, b_aux2, 0.5)
+                     a_fin := linear_interpolation(a_aux1, a_aux2, 0.5)
 
-                            nr += uint32(proc_h * proc_w * float64(r))
-                            ng += uint32(proc_h * proc_w * float64(g))
-                            nb += uint32(proc_h * proc_w * float64(b))
-                            na += uint32(proc_h * proc_w * float64(a))
-
-                            // nr = min(255 << 8 - 1, nr + uint32(proc_h * proc_w * float64(r)))
-                            // ng = min(255 << 8 - 1, ng + uint32(proc_h * proc_w * float64(g)))
-                            // nb = min(255 << 8 - 1, nb + uint32(proc_h * proc_w * float64(b)))
-                            // na = min(255 << 8 - 1, na + uint32(proc_h * proc_w * float64(a)))
-
-                            ret.(draw.Image).Set(int(i), int(j), color.RGBA{uint8(nr >> 8), uint8(ng >> 8), uint8(nb >> 8), uint8(na >> 24)})
-                        }
-                    }
+                     ret.(draw.Image).Set(x, y, color.RGBA{uint8(r_fin >> 8), uint8(g_fin >> 8), uint8(b_fin >> 8), uint8(a_fin >> 8)})
                 }
             }
     //     } ()
@@ -122,21 +118,21 @@ func main() {
   defer img_file.Close()
   img, _, _ := image.Decode(img_file)
 
-  card := image.NewRGBA(image.Rect(0, 0, 800, 800))
-  draw.Draw(card, card.Bounds(), img, image.Point{0, 0}, draw.Src)
+  // card := image.NewRGBA(image.Rect(0, 0, 800, 800))
+  // draw.Draw(card, card.Bounds(), img, image.Point{0, 0}, draw.Src)
 
   // f_elem := float64(1) / 9
   // f := Filter { [3][3]float64 { {f_elem, f_elem, f_elem}, {f_elem, f_elem, f_elem}, {f_elem, f_elem, f_elem}}}
 
   // apply_filter(card, image.Point {10, 10}, image.Point {100, 100}, f)
 
-  card2 := image.NewRGBA(image.Rect(0, 0, 800, 800))
-  resize(card, card2)
+  card := image.NewRGBA(image.Rect(0, 0, 1000, 400))
+  resize(img, card)
 
   fmt.Println("done!")
 
   rez, _ := os.Create("new.jpg")
   defer rez.Close()
 
-  jpeg.Encode(rez, card2, &jpeg.Options{jpeg.DefaultQuality})
+  jpeg.Encode(rez, card, &jpeg.Options{jpeg.DefaultQuality})
 }
