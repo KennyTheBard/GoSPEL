930c2706cc7c2b7ea997b85f67c75baef25f42b6
Octavian-Alexandru ȚECULESCU (87554) <octavian.teculescu@stud.acs.upb.ro>
2019-01-07 21:38:48 +0200

    Added timer for testing and free term in color channel polynomial function

--- handler.go
+++ handler.go
@@ -3,37 +3,27 @@ package main
 import (
     "fmt"
     "os"
+    "time"
     "image"
     "image/jpeg"
     //"image/png"
-    "image/color"
     lib "./lib"
 )
 
 func main() {
-    img_file, _ := os.Open("test.jpg")
+    img_file, _ := os.Open("humans.jpg")
     defer img_file.Close()
     img, _, _ := image.Decode(img_file)
 
-    // card := image.NewRGBA(image.Rect(0, 0, 800, 800))
-    // draw.Draw(card, card.Bounds(), img, image.Point{0, 0}, draw.Src)
+    start := time.Now()
 
-    f_elem := float64(1) / 9
-    f := lib.Filter { [][]float64 { {f_elem, f_elem, f_elem}, {f_elem, f_elem, f_elem}, {f_elem, f_elem, f_elem}}}
+    card := lib.Copy_image(img)
+    lib.Modify_colors(card, [4][5]float64{{-1, 0, 0, 0, (256 << 8) - 1}, {0, -1, 0, 0, (256 << 8) - 1}, {0, 0, -1, 0, (256 << 8) - 1}, {0, 0, 0, 1}})
 
-    card := lib.Rescale(img, image.Rect(0, 0, 800, 800))
+    elapsed := time.Since(start)
 
-    lib.Apply_filter(card, image.Point {150, 150}, image.Point {650, 650}, f, 5)
+    fmt.Printf("Done in %s!\n", elapsed)
 
-    lib.Modify_colors(card, [4][4]float64{{0.35, 0, 0, 0}, {0.35, 0, 0, 0}, {0.35, 0.35, 0.3, 0}, {0, 0, 0, 1}})
-
-    grd := lib.Linear_gradient(image.Rect(0, 0, 800, 800), []int{0, 700}, []color.Color{color.RGBA{255, 255, 255, 255}, color.RGBA{255, 0, 0, 125}})
-
-    card = lib.Merge(card, grd, image.Rect(0, 0, 800, 800))
-
-    card = lib.Rotate(card, 20)
-
-    fmt.Println("done!")
     rez, _ := os.Create("new.jpg")
     defer rez.Close()
 
