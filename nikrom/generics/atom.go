6f838d0762da14b6c16ac3bccdaff63da0fe2f6c
KennyTheBard <octav_teculescu@yahoo.com>
2019-04-11 12:52:46 +0300

    Organized the code a little

---
+++ nikrom/generics/atom.go
@@ -0,0 +1,38 @@
+package generics
+
+/**
+ *	A general interface to be passed up in the Interpreter Tree
+ *	and given as arguments to internal functions.
+ */
+type Void interface {}
+
+/**
+ *	Takes place of optional arguments and provides support for
+ *	generic data types passed as arguments.
+ */
+type Arguments struct {
+	args []Void
+}
+
+/**
+ *	Serves as template for all image processing functions
+ *	provided by the program.
+ */
+type ImageProcessing func(Arguments) Void
+
+/**
+ *	Structure used for source code parsing.
+ */
+type Atom struct {
+	proc string
+	subatoms []Atoms
+}
+
+/**
+ *	Evaluates the curret expression and calls a function on given
+ *	arguments.
+ */
+func (atom *Atom) Interpret() Void {
+	// TODO: move this function to another file
+	return nil
+}
